0a4ef019d241845dce855d5cf118a4fdb2fefbd2
Andres Felipe Pabon <afpabon@yahoo.com>
2017-03-03 10:24:07 -0500

    added markerclusterer.js

--- client/main.js
+++ client/main.js
@@ -133,6 +133,7 @@ Meteor.startup(function() {
     v : '3',
     key : 'AIzaSyCD75m_UjRrRBpFbDNcW6N3lLx6P316O1U'
   });
+  GoogleMaps.loadUtilityLibrary('/scripts/markerclusterer.js');
 });
 
 Template.body.helpers({
